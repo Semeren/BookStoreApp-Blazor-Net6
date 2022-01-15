aeb542a058ab8ec74f271f27d7caacef671c5923
Justin Hack <jhack@theamegroup.com>
2022-01-15 00:16:08 -0500

    Added EFCore and Scaffolded DB

---
+++ BookStoreDb/dbo/Tables/Books.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Books]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NVARCHAR(50) NULL, 
+    [Year] INT NULL, 
+    [ISBN] NVARCHAR(50) NULL UNIQUE,
+    [Summary] NVARCHAR(250) NULL, 
+    [Image] NVARCHAR(50) NULL, 
+    [Price] DECIMAL(18, 2) NULL, 
+    [AuthorId] INT NULL, 
+    CONSTRAINT [FK_Books_ToAuthors] FOREIGN KEY ([AuthorId]) REFERENCES [Authors]([Id])
+)
