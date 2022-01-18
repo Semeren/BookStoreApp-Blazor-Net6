a929a3c206d1e0c490e5c6d53d2fb9dfbb1863c1
Justin Hack <jhack@theamegroup.com>
2022-01-17 23:10:10 -0500

    Started building out Author pages.
    Added index and create with new error handling and service clients for api requests.

---
+++ BookStoreApp.Blazor.Server.UI/Services/AuthorService.cs
@@ -0,0 +1,54 @@
+﻿using Blazored.LocalStorage;
+using BookStoreApp.Blazor.Server.UI.Services.Base;
+
+namespace BookStoreApp.Blazor.Server.UI.Services
+{
+    public class AuthorService : BaseHttpService, IAuthorService
+    {
+        private readonly IClient client;
+
+        public AuthorService(IClient client, ILocalStorageService localStorage) : base(client, localStorage)
+        {
+            this.client = client;
+        }
+
+        public async Task<Response<int>> CreateAuthor(AuthorCreateDto author)
+        {
+            Response<int> response = new();
+            
+            try
+            {
+                await GetBearerToken();
+                await client.AuthorsPOSTAsync(author);
+            }
+            catch (ApiException ex)
+            {
+
+                response = ConvertApiExceptions<int>(ex);
+            }
+            return response;
+        }
+
+        public async Task<Response<List<AuthorReadOnlyDto>>> GetAuthors()
+        {
+            Response<List<AuthorReadOnlyDto>> response;
+
+            try
+            {
+                await GetBearerToken();
+                var data = await client.AuthorsAllAsync();
+                response = new Response<List<AuthorReadOnlyDto>>
+                {
+                    Data = data.ToList(),
+                    Success = true
+                };
+            }
+            catch (ApiException ex)
+            {
+                response = ConvertApiExceptions<List<AuthorReadOnlyDto>>(ex);
+            }
+
+            return response;
+        }
+    }
+}
