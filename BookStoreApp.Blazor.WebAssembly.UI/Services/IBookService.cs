6f3741e8c5c21d5230a315bb9ec01314e99160c3
Justin Hack <jhack@theamegroup.com>
2022-02-06 22:06:43 -0500

    Web Assembly Version

---
+++ BookStoreApp.Blazor.WebAssembly.UI/Services/IBookService.cs
@@ -0,0 +1,18 @@
+﻿using BookStoreApp.Blazor.WebAssembly.UI.Services.Base;
+
+namespace BookStoreApp.Blazor.WebAssembly.UI.Services
+{
+    public interface IBookService
+    {
+        Task<Response<List<BookReadOnlyDto>>> GetAll();
+
+        Task<Response<BookDetailsDto>> Get(int Id);
+        Task<Response<BookUpdateDto>> GetForUpdate(int Id);
+
+        Task<Response<int>> Create(BookCreateDto book);
+        Task<Response<int>> Edit(int id, BookUpdateDto autbookhor);
+        Task<Response<int>> Delete(int id);
+
+
+    }
+}
